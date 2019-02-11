14fcb7d42784afcbcf2b5d2b9e8e702043e51d7f
Rushani Shawuti <ruhsane924@gmail.com>
2019-02-10 21:58:34 -0800

    users can submit and save posts

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ const exphbs  = require('express-handlebars');
 const app = express()
 const port = 3000
 
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Add after body parser initialization!
+app.use(expressValidator());
+
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
@@ -14,5 +25,10 @@ app.get('/', function (req, res) {
 app.get('/posts/new', function (req, res) {
     res.render('posts-new', {});
 });
+
+require('./controllers/posts.js')(app);
+
+// Set db
+require('./data/reddit-db');
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
