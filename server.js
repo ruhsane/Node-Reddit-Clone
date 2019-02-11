46feede88aaeff8d31bd41325f6f31cb8c563142
Rushani Shawuti <ruhsane924@gmail.com>
2019-02-10 21:12:11 -0800

    shows form to create post

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
-    res.render('home');
+    res.render('home', {msg: 'Reddit.js'});
+});
+
+app.get('/posts/new', function (req, res) {
+    res.render('posts-new', {});
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
