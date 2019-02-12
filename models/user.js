ca79232dbbefd3e047d059a60054ed27867b85a7
Rushani Shawuti <ruhsane924@gmail.com>
2019-02-11 20:31:44 -0800

    users can sign up with secured password

---
+++ models/user.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    createdAt: { type: Date },
+    updatedAt: { type: Date },
+    password: {type: String, select:false},
+    username:{type:String, required:true}
+});
+
+
+//define the callback with a regular function to avoid problems with this
+UserSchema.pre("save", function(next) {
+    //set createdAt AND updatedAt
+    const now = new Date();
+    this.updatedAt = now;
+    if (!this.createdAt) {
+        this.createdAt = now;
+    }
+
+    //encrypt password
+    const user = this;
+    if (!user.isModified("password")) {
+        return next();
+    }
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+//need to use function to enable this.password to work
+UserSchema.methods.comparePassword = function(password, done) {
+    bcrypt.compare(password, this.password, (err, isMatch) => {
+        done(err, isMatch);
+    });
+};
+
+module.exports = mongoose.model("User", UserSchema)
--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-const express = require('express')
+require('dotenv').config();
+var cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+const express = require('express');
 const exphbs  = require('express-handlebars');
 
 const app = express()
@@ -14,12 +17,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Add after body parser initialization!
 app.use(expressValidator());
 
+app.use(cookieParser()); 
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 require('./controllers/posts.js')(app);
 require('./controllers/comments.js')(app);
+require('./controllers/auth.js')(app);
 // Set db
 require('./data/reddit-db');
 
