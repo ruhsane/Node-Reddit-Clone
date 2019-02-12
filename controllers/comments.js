45c2717babfaf35f4ec2ee79ff94215303df040d
Rushani Shawuti <ruhsane924@gmail.com>
2019-02-11 22:18:55 -0800

    authors for posts are visible

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,30 +1,42 @@
 const Post = require('../models/post');
+const User = require('../models/user');
 const Comment = require('../models/comment');
 
 module.exports = function(app) {
 
 //create comment
-app.post("/posts/:postId/comments", function(req, res) {
-    //instantiate instance of model
-    const comment = new Comment(req.body);
+app.post("/posts/:postId/comments", (req, res) => {
 
-    //save instance of Comment model to DB
-    comment
-        .save()
-        .then(comment => {
-            return Post.findById(req.params.postId);
-        })
-        .then(post => {
-            //unshift adds an element to the front of an array, while push adds it to the end
-            post.comments.unshift(comment);
-            return post.save();
-        })
-        .then(post => {
-            res.redirect('/');
-        })
-        .catch(err => {
-            console.log(err);
-        });
+    if (req.user) {
+        //instantiate instance of model
+        const comment = new Comment(req.body);
+        comment.author = req.user._id;
+
+        //save instance of Comment model to DB
+        comment
+            .save()
+            .then(comment => {
+                return Promise.all([
+                  Post.findById(req.params.postId)
+                ]);
+            })
+            .then(([post, user]) => {
+                //unshift adds an element to the front of an array, while push adds it to the end
+                post.comments.unshift(comment);
+                return Promise.all([
+                  post.save()
+                ])
+            })
+            .then(post => {
+                res.redirect(`/posts/${req.params.postId}`);
+
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    } else {
+        return res.status(401).send({ message: "Unauthorized! Please log in first." });
+    }
 });
 
 };
--- controllers/posts.js
+++ controllers/posts.js
@@ -1,12 +1,13 @@
-const Post = require('../models/post')
+const Post = require('../models/post');
+const User = require('../models/user');
 
 module.exports = app => {
 
-//get posts in main page
+//INDEX. get posts in main page
   app.get('/', (req, res) => {
     var currentUser = req.user;
-
-    Post.find({})
+    console.log(req.cookies);
+    Post.find().populate('author')
         .then(posts => {
             res.render("posts-index", { posts, currentUser });
         })
@@ -15,24 +16,36 @@ module.exports = app => {
         })
   })
 
-  //get form
+  //get new post form
   app.get("/posts/new", (req, res) => {
       var currentUser = req.user;
 
       res.render('posts-new', { currentUser });
   })
 
-  // CREATE
+  // CREATE new post
   app.post("/posts/new", (req, res) => {
 
       if (req.user) {
           //INSTANTIATE INSTANCE OF POST MODEL
           const post = new Post(req.body);
+          post.author = req.user._id;
 
           //SAVE INSTANCE OF POST MODEL TO DB
-          post.save(function(err, post) {
-              //REDIRECT TO THE ROOT
-              return res.redirect('/');
+          post
+            .save()
+            .then(post => {
+                return User.findById(req.user._id);
+            })
+            .then(user => {
+                user.posts.unshift(post);
+                user.save();
+                //redirect to the new post
+                res.redirect(`/posts/${post._id}`);
+
+            })
+            .catch( err => {
+                console.log(err.message);
           });
       } else {
           return res.status(401).send({ message: "Unauthorized! Please log in first." });
@@ -40,12 +53,12 @@ module.exports = app => {
       }
   });
 
-//each post
+//SHOW each post
   app.get("/posts/:id", function(req, res) {
       var currentUser = req.user;
 
       //LOOK UP THE Post
-      Post.findById(req.params.id).populate('comments')
+      Post.findById(req.params.id).populate('comments').populate('author')
         .then(post => {
             res.render("posts-show", { post, currentUser });
         })
@@ -58,7 +71,7 @@ module.exports = app => {
     app.get("/n/:subreddit", function(req,res) {
         var currentUser = req.user;
 
-        Post.find({subreddit: req.params.subreddit})
+        Post.find({subreddit: req.params.subreddit}).populate('author')
             .then(posts => {
                 res.render("posts-index", {posts, currentUser});
             })
